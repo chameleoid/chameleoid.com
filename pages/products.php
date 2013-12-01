c6cf054fa024fbf9e17e69f990d4095434b29a38
rummik <r@9k1.us>
2013-11-30 20:08:19 -0500

    Add shellcast to products page

--- pages/products.php
+++ pages/products.php
@@ -5,3 +5,9 @@
 
 <a href="https://telepathyjs.com/">site</a>, 
 <a href="https://github.com/chameleoid/telepathy/">source</a>
+
+<h3>Shellcast</h3>
+<p>A shellcasting client and server.</p>
+
+<a href="http://ansible.tk/">site</a>,
+<a href="https://github.com/chameleoid/shellcast/">source</a>
