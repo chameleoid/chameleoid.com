a76c08bfcddbbaf7503214577db88a31c5e4e6d5
rummik <r@9k1.us>
2013-11-30 20:12:07 -0500

    Fix spacing issues slightly

--- pages/products.php
+++ pages/products.php
@@ -3,11 +3,11 @@
 <h3>Telepathy</h3>
 <p>A telepathic password manager which generates passwords using a shared secret.</p>
 
-<a href="https://telepathyjs.com/">site</a>, 
-<a href="https://github.com/chameleoid/telepathy/">source</a>
+<p><a href="https://telepathyjs.com/">site</a>, 
+<a href="https://github.com/chameleoid/telepathy/">source</a></p>
 
 <h3>Shellcast</h3>
 <p>A shellcasting client and server.</p>
 
-<a href="http://ansible.tk/">site</a>,
-<a href="https://github.com/chameleoid/shellcast/">source</a>
+<p><a href="http://ansible.tk/">site</a>,
+<a href="https://github.com/chameleoid/shellcast/">source</a></p>
