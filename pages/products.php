dc01c3b83cacbe8ca945a85b45bc71a47a8f040b
rummik <r@9k1.us>
2014-12-03 12:44:03 -0500

    Update links to Telepathy and Shellcast

--- pages/products.php
+++ pages/products.php
@@ -3,11 +3,11 @@
 <h3>Telepathy</h3>
 <p>A telepathic password manager which generates passwords using a shared secret.</p>
 
-<p><a href="https://telepathyjs.com/">site</a>, 
+<p><a href="https://telepathy.pw/">site</a>, 
 <a href="https://github.com/chameleoid/telepathy/">source</a></p>
 
 <h3>Shellcast</h3>
 <p>A shellcasting client and server.</p>
 
-<p><a href="http://shellcast.tk/">site</a>,
+<p><a href="http://tty.tv/">site</a>,
 <a href="https://github.com/chameleoid/shellcast/">source</a></p>
