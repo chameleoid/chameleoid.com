5561029d9feaa5f74f0e2b53bc6f51b59ce93ace
rummik <k@9k1.us>
2017-10-05 18:17:51 -0400

    Add domains.php

---
+++ pages/domains.php
@@ -0,0 +1,20 @@
+<h3>Actively Used Domains</h3>
+Domains we own and actively use:
+
+<ul>
+	<li>chameleoid.com</li>
+	<li>telepathyjs.com</li>
+	<li>telepathy.pw</li>
+	<li>tty.tv</li>
+</ul>
+
+<h3>Currently Idle Domains</h3>
+Domains we own but aren't actively using for anything just yet:
+
+<ul>
+	<li>awfulzen.com</li>
+	<li>vira.so</li>
+	<li>rpg.im</li>
+</ul>
+
+<p><small>Last updated: <?=date('m/d/Y', filemtime(__FILE__));?> &mdash; <a href="https://github.com/chameleoid/chameleoid.com/commits/master/pages/domains.php">History</a></small></p>
